a924f59e58c1a7b6f794d5f83e9c91f56c7e60f7
Alyssa Horrocks <alyssa.horrocks2@gmail.comcom>
2018-04-30 20:10:37 -0700

    add inquirer, remove yargs

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const program = require('commander');
 const argv = require('yargs').argv;
 
+let store = [];
+
 program
   .version('0.0.1')
   .description('Groceries are fun');
@@ -11,6 +13,15 @@ program
   .command('test')
   .action(() => {
     console.log('test');
+  });
+
+program
+  .command('add <itemName>')
+  .description('Add a product to your store')
+  .action((itemName) => {
+    const newItem = { itemName };
+    store.push(newItem);
+    console.log(store);
   });
 
 program.parse(process.argv)
