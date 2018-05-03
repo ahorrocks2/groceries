b310173c16d788fd3626d83c5b48ed14b5eebb83
Alyssa Horrocks <alyssa.horrocks2@gmail.comcom>
2018-05-02 22:01:19 -0700

    validation on int inputs

--- itemQuestions.js
+++ itemQuestions.js
@@ -7,11 +7,27 @@ module.exports = [
   {
     type: 'input',
     name: 'quantity',
-    message: `What quantity would you like to add to the cart?`
+    message: `What quantity would you like to add to the cart?`,
+    validate: value => {
+      const input = (+value).toFixed();
+      if(isNaN(input) || input < 1) {
+        console.log(' Please input a number greater than 1 as a quantity.');
+        return false;
+      } 
+      return true;
+    }
   },
   {
     type: 'input',
     name: 'price',
-    message: 'How much does one cost?'
+    message: 'How much does one cost?',
+    validate: value => {
+      const input = (+value).toFixed();
+      if(isNaN(input) || input < 1) {
+        console.log(' Please input a number greater than 1 as a price.');
+        return false;
+      } 
+      return true;
+    }
   }
 ];
